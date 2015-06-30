3ab06f8792de19fb93bc28981c0ae676003738e8
Kelvin Tran <mrkelvintran@gmail.com>
2015-06-30 17:24:47 +1000

    Update chloro-map.js

--- static/js/chloro-map.js
+++ static/js/chloro-map.js
@@ -1,12 +1,12 @@
 var map = d3.geomap.choropleth()
     .geofile('foreign-lawyers/USA.json')
     .projection(d3.geo.albersUsa)
-    .column('2012')
+    .column('2014')
     .unitId('fips')
     .scale(1000)
     .legend(true);
 
-d3.csv('foreign-lawyers/venture-capital.csv', function(error, data) {
+d3.csv('foreign-lawyers/aus-states.csv', function(error, data) {
     d3.select('#map')
         .datum(data)
         .call(map.draw, map);
